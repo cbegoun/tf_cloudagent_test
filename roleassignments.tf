f87e6c6843f77648a5f5ecbac3303dcc7e9a7c14
cbegoun <begouncody@gmail.com>
2024-12-03 12:51:49 -0700

    adding reader assignment as test

--- roleassignments.tf
+++ roleassignments.tf
@@ -13,3 +13,9 @@ resource "azurerm_role_assignment" "mi_contributor" {
   role_definition_id = "${data.azurerm_subscription.current.id}${data.azurerm_role_definition.contributor.id}"
   principal_id       = azurerm_container_group.tf_agent_cg.identity[0].principal_id
 }
+
+resource "azurerm_role_assignment" "mi_reader" {
+  scope                = data.azurerm_subscription.current.id
+  role_definition_name = "Reader"
+  principal_id         = azurerm_container_group.tf_agent_cg.identity[0].principal_id
+}

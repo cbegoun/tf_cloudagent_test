748b5b7d517aef72808d0e2a33de2ca3f06d7f67
cbegoun <begouncody@gmail.com>
2024-12-03 12:47:25 -0700

    changing role definition id to name

--- roleassignments.tf
+++ roleassignments.tf
@@ -3,9 +3,9 @@ data "azurerm_role_definition" "contributor" {
 }
 
 resource "azurerm_role_assignment" "mi_tf_customrole" {
-  scope              = data.azurerm_subscription.current.id
-  role_definition_id = azurerm_role_definition.TF_CustomRole.id
-  principal_id       = azurerm_container_group.tf_agent_cg.identity[0].principal_id
+  scope                = data.azurerm_subscription.current.id
+  role_definition_name = "TF_CustomRole"
+  principal_id         = azurerm_container_group.tf_agent_cg.identity[0].principal_id
 }
 
 resource "azurerm_role_assignment" "mi_contributor" {

6d335c34a64dac5a0f3232f52d2a3cd3904a6407
cbegoun <begouncody@gmail.com>
2024-12-03 12:07:24 -0700

    adding subnet delegation for container group

--- main.tf
+++ main.tf
@@ -16,4 +16,13 @@ resource "azurerm_subnet" "internal" {
   resource_group_name  = azurerm_resource_group.tf_agent_rg.name
   virtual_network_name = azurerm_virtual_network.tf_agent_vn.name
   address_prefixes     = ["10.0.2.0/24"]
+
+  delegation {
+    name = "cg_delegation"
+
+    service_delegation {
+      name    = "Microsoft.ContainerInstance/containerGroups"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
+    }
+  }
 }
